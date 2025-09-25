3187f5f8f0d514848f784ad5c1587d3de5f6aa39
danielo515 <rdanielo@gmail.com>
2025-09-25 09:59:37 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.alejandra);
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            nodePackages.pnpm
+            git
+          ];
+          
+          shellHook = ''
+            echo "Node.js: $(node --version)"
+            echo "pnpm: $(pnpm --version)"
+            echo "Git: $(git --version)"
+            echo ""
+          '';
+        };
+      });
+    };
+}
